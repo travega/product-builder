452357e1cacfa59a0c4d2a938fcdcf18fbd666ae
Andrew Scott <andrewscott@andrewsc-ltm.internal.salesforce.com>
2019-01-29 21:46:02 +0000

    update

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
 jQuery(document).ready(function ($) {
 	if ($(this).width() < 1024) {
 		var el = $(".tot-price > .total");
-		if (el) {
+		if (el.length) {
 			el.addClass("total-muted");
 			el.removeClass("total");
 		}
 
 		var buy = $(".buy > .total-muted");
-		if (buy) {
+		if (buy.length) {
 			buy.removeClass("total-muted");
 			buy.addClass("total");
 		}
 	} else {
 		var el = $(".tot-price > .total-muted");
-		if (el) {
+		if (el.length) {
 			el.removeClass("total-muted");
 			el.addClass("total");
 		}
 
-		var buy = $(".buy > .total-muted");
-		if (buy) {
+		var buy = $(".buy > .total");
+		if (buy.length) {
 			buy.addClass("total-muted");
 			buy.removeClass("total");
 		}
